4f2c940001d77aa858ee9329b46e77063a2e2479
Leonidus786 <abhi.upadh@gmail.com>
2025-08-08 12:29:59 +0530

    Numerics and Strings

---
+++ Day_3/Numeric_types.py
@@ -0,0 +1,52 @@
+## Data Types
+
+
+# Numeric Type
+
+a = 4  ## Integer
+
+b = 3.589   # Float -- decimals
+
+c = 5 + 6j  # Complex Number
+
+#print(a)
+#print(type(a))
+
+#print(b)
+#print(type(b))
+
+#print(c)
+#print(type(c))
+
+
+## Changing the data type -- type casting -- to convert from one data type to another..
+
+# Integers to float
+
+# d = float(a)
+
+# print(d)
+
+#e = int(b)
+#print(e)
+
+# Can we convert complex numbers.
+# We can't typecast complex numbers
+
+#f = int(c)
+#f = float(c)
+
+
+#print(f)
+
+
+
+#g = complex(a)
+#print(g)
+
+h = complex(b)
+print(h)
+
+
+
+
---
+++ Day_3/Strings.py
@@ -0,0 +1,38 @@
+## Anything between double quotes(" ") or single quotes (' ') or triple quotes (""" """)(''' ''') is treated as string in python.
+
+
+Full_name = "Saad Zeeshan"
+
+full_Name = 'Saad Zeeshan'
+
+FULL_name = '''Saad Zeeshan'''
+
+#print(Full_name)
+#print(type(Full_name))
+
+#print(full_Name)
+#print(type(full_Name))
+
+#print(FULL_name)
+#print(type(FULL_name))
+
+
+a = 'a'
+
+b = '10'
+
+c = "16 + 5j"
+
+
+#print(type(c))
+
+
+#f_name = int(Full_name)
+
+#print(f_name)
+
+#print(b)
+
+#e = int(b)
+
+#print(e)
