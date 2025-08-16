8f046fc444302e59c1c8a42b717371610f4886ea
Leonidus786 <abhi.upadh@gmail.com>
2025-08-16 10:37:56 +0530

    input function

---
+++ Day_4/input_function.py
@@ -0,0 +1,21 @@
+
+# Input function -- input("Please Give your prompt")
+
+## If you want to give some value to anything ypou will use input() in python for this.
+
+# print("Saad")
+
+
+# Name = input("What is your name : ")
+
+
+# enter_a_number = int(input("Please give a number : "))
+
+
+# someone says  I want to fill my name on my own
+
+
+# Name = input("")
+
+
+Phone_Number = int(input("please provide your mobile number : "))
