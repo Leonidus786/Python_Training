2ba1d3442d0d844079ea70b917caf2988db08b52
Leonidus786 <abhi.upadh@gmail.com>
2025-08-19 12:42:07 +0530

    for loop

--- Day_5/Driving_Licence.py
+++ Day_5/Driving_Licence.py
@@ -3,12 +3,12 @@
 Age = int(input("Enter your age: "))
 
 
-#if Age >= 18 and Age < 55:
-#    print("Allowed to make a driving license")
-#elif Age>=12 and Age<18 :
-#    print("you are a teenager")
-#elif Age>= 55:
-#    print("You can't have a driving license")
+if Age >= 18 and Age < 55:
+    print("Allowed to make a driving license")
+elif Age>=12 and Age<18 :
+    print("you are a teenager")
+elif Age>= 55:
+    print("You can't have a driving license")
 else:
     print("you are a kid")
  
---
+++ Day_6/for_loop.py
@@ -0,0 +1,37 @@
+
+
+Name = "Saad Zeeshan"
+
+a = 10
+
+b = 3.5
+
+c = 3+5j
+
+d = True
+
+e = False
+
+
+
+list_1 = [10, 3.5, 3+5j, True, False, "Saad Zeeshan"]
+
+#print(list_1)
+
+for i in list_1:
+    print(i)
+
+
+
+
+
+
+
+
+print()
+print()
+print()
+print()
+print()
+#for a in Name:
+#    print(a)
