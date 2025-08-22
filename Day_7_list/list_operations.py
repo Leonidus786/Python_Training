707fe2f4a476998e38f0788b3b0414194f03bd2d
Leonidus786 <abhi.upadh@gmail.com>
2025-08-22 12:30:12 +0530

    List_operations

---
+++ Day_7_list/list_operations.py
@@ -0,0 +1,91 @@
+
+
+#list_1 = [True, False, 0, "Saad", "Abhishek", 22.7, 3+5j]
+
+#print(list_1)
+
+#a = 10
+
+#print(type(a))
+
+#print(type(list_1))
+
+#print(list_1[3])
+#print(list_1[-4])
+
+#Slicing -- list_name[start:End]
+
+#print(list_1[0:2])
+
+#print(list_1[3:6])
+
+#print(list_1[-4:-1])
+
+#print(list_1[1:])
+
+#print(list_1[:3])
+
+#list_1[1] = "Apple"
+
+#print(list_1)
+
+#list_1[-5] = "Cherry"
+# print(list_1)
+
+
+#thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+#thislist[1:3] = ["blackcurrant", "watermelon"]
+#print(thislist)
+
+
+#thislist = ["apple", "banana", "cherry"]
+#thislist[1:2] = ["blackcurrant", "watermelon"]
+#print(thislist)
+
+#Note: The length of the list will change when the number of items inserted does not match the number of items replaced.
+
+#If you insert less items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
+
+
+list_1 = [True, False, 0, "Saad", "Abhishek", 22.7, 3+5j]
+#print(len(list_1))
+
+
+
+#thislist = ["apple", "banana", "cherry"]
+#print(len(thislist))
+#thislist[1:3] = ["watermelon"]
+#print(thislist)
+
+
+
+
+#print(len(thislist))
+
+#list_1.insert(2, "Amit")
+#print(list_1)
+
+
+list_1.append("Orange")
+
+#print(list_1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
